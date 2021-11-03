89ea6cd4b154f645636f856cb15d9c680b29740e
seonghun <dev.seonghun@gmail.com>
2021-11-03 22:21:42 +0900

    change some error

--- week9/linearRegression.py
+++ week9/linearRegression.py
@@ -7,7 +7,7 @@ import statsmodels.api as sm
 from matplotlib import pyplot as plt
 
 
-def load_dbsocore_data():
+def load_dbscore_data():
     conn = pymysql.connect(host="localhost", user="root", password="6674", db='dataSceince')
     curs = conn.cursor(pymysql.cursors.DictCursor)
 
@@ -27,11 +27,11 @@ def load_dbsocore_data():
     return x, y
 
 
-x, y = load_dbsocore_data()
+X, y = load_dbscore_data()
 
-x_const = sm.add_constant(x)
+X_const = sm.add_constant(X)
 
-model = sm.OLS(y, x_const)
+model = sm.OLS(y, X_const)
 ls = model.fit()
 
 print(ls.summary())
@@ -39,14 +39,13 @@ print(ls.summary())
 ls_c = ls.params[0]
 ls_m = ls.params[1]
 
-y_pred = ls_m * x + ls_c
-
-plt.scatter(x, y)
-plt.plot([min(x), max(x)], [min(y_pred), max(y_pred)], color='red')
+y_pred = ls_m * X + ls_c
+plt.scatter(X, y)
+plt.plot([min(X), max(X)], [min(y_pred), max(y_pred)], color='red')
 plt.show()
 
 
-def gradient_descent_naive(x, y):
+def gradient_descent_naive(X, y):
     epochs = 100000
     min_grad = 0.0001
     learning_rate = 0.001
@@ -59,36 +58,40 @@ def gradient_descent_naive(x, y):
     c_grad = 0.0
     m_grad = 0.0
 
-    for epochs in range(epochs):
+    for epoch in range(epochs):
+
         for i in range(n):
-            y_pred = m * x[i] + c  # 현재 i번째 샘플에 대한 예측값
-            m_grad += 2 * (y_pred - y[i]) * x[i]
-            c_grad = 2 * (y_pred - y[i])
+            y_pred = m * X[i] + c
+            m_grad += 2 * (y_pred - y[i]) * X[i]
+            c_grad += 2 * (y_pred - y[i])
+
         c_grad /= n
         m_grad /= n
 
         m = m - learning_rate * m_grad
         c = c - learning_rate * c_grad
 
-        if epochs % 1000 == 0:
-            print("epoch : %d m_grad = %f c_grad=%f m=%f c=%f" % (epochs, m_grad, c_grad, m, c))
-        if abs(m_grad) < min_grad and abs(c_grad) < min_grad:
+        if (epoch % 1000 == 0):
+            print("epoch %d: m_grad=%f, c_grad=%f, m=%f, c=%f" % (epoch, m_grad, c_grad, m, c))
+
+        if (abs(m_grad) < min_grad and abs(c_grad) < min_grad):
             break
+
     return m, c
 
 
 start_time = time.time()
-m, c = gradient_descent_naive(x, y)
+m, c = gradient_descent_naive(X, y)
 end_time = time.time()
 
 print("%f seconds" % (end_time - start_time))
 
-print("\n\n final:")
-print("gd_m = %f, gd_c=%f" % (m, c))
-print("ls_m = %f ls_c = %f" % (ls_m, ls_c))
+print("\n\nFinal:")
+print("gdn_m=%f, gdn_c=%f" % (m, c))
+print("ls_m=%f, ls_c=%f" % (ls_m, ls_c))
 
 
-def gradient_descent_vector(x, y):
+def gradient_descent_vectorized(X, y):
     epochs = 100000
     min_grad = 0.0001
     learning_rate = 0.001
@@ -101,27 +104,30 @@ def gradient_descent_vector(x, y):
     c_grad = 0.0
     m_grad = 0.0
 
-    for epochs in range(epochs):
+    for epoch in range(epochs):
 
-        y_pred = m * x + c
-        m_grad = (2 * (y_pred - y) * x).sum() / n
+        y_pred = m * X + c
+        m_grad = (2 * (y_pred - y) * X).sum() / n
         c_grad = (2 * (y_pred - y)).sum() / n
 
         m = m - learning_rate * m_grad
         c = c - learning_rate * c_grad
 
-        if epochs % 1000 == 0:
-            print("epoch : %d m_grad = %f c_grad=%f m=%f c=%f" % (epochs, m_grad, c_grad, m, c))
-        if abs(m_grad) < min_grad and abs(c_grad) < min_grad:
+        if (epoch % 1000 == 0):
+            print("epoch %d: m_grad=%f, c_grad=%f, m=%f, c=%f" % (epoch, m_grad, c_grad, m, c))
+
+        if (abs(m_grad) < min_grad and abs(c_grad) < min_grad):
             break
+
     return m, c
 
+
 start_time = time.time()
-m, c = gradient_descent_vector(x, y)
+m, c = gradient_descent_vectorized(X, y)
 end_time = time.time()
 
 print("%f seconds" % (end_time - start_time))
 
-print("\n\n final:")
-print("gdv_m = %f, gdv_c=%f" % (m, c))
-print("lsv_m = %f lsv_c = %f" % (ls_m, ls_c))
+print("\n\nFinal:")
+print("gdv_m=%f, gdv_c=%f" % (m, c))
+print("ls_m=%f, ls_c=%f" % (ls_m, ls_c))
