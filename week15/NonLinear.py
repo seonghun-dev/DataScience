c99e40394083709d773594bc0b13558a3a124a46
seonghun <dev.seonghun@gmail.com>
2021-12-14 10:02:39 +0900

    add None Linear Regression using tf

---
+++ week15/NonLinear.py
@@ -0,0 +1,71 @@
+import math
+import random
+
+import numpy as np
+from keras import Input
+from keras.layers import Dense
+from keras.models import Sequential
+from tensorflow import initializers
+
+
+def gen_sequential_model():
+    model = Sequential([
+        Input(4, name='input_layer'),
+        Dense(16, activation='sigmoid', name='hidden_layer1', kernel_initializer=initializers.RandomNormal(mean=0.0, stddev=0.05, seed=42)),
+        Dense(1, activation='relu', name='output_layer', kernel_initializer=initializers.RandomNormal(mean=0.0, stddev=0.05, seed=42))
+    ])
+
+    model.summary()
+    model.compile(optimizer='sgd', loss='mse')
+
+    return model
+
+
+def gen_linear_regression_dateset(numofsamples=500, w1=3, w2=5, w3=12, w4=20, b=10):
+    np.random.seed(42)
+    x_arr = []
+    x_arr_cal = []
+    for i in range(numofsamples):
+        x_arr.append([random.random(), random.random(), random.random(), random.random()])
+        x_arr_cal.append([x_arr[i][0], math.pow(x_arr[i][1], 2), math.pow(x_arr[i][2], 3), math.pow(x_arr[i][3], 4)])
+
+    X = np.array(x_arr)
+    coef = np.array([w1, w2, w3, w4])
+
+    bias = b
+    y = np.matmul(np.array(x_arr_cal), coef.transpose()) + bias
+    return X, y
+
+
+def plot_loss_curve(history):
+    import matplotlib.pyplot as plt
+
+    plt.figure(figsize=(15, 10))
+
+    plt.plot(history.history['loss'][1:])
+    plt.plot(history.history['val_loss'][1:])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper right')
+    plt.show()
+
+
+def predict_new_sample(model, x, w1=3, w2=5, w3=12, w4=20, b=10):
+    x = x.reshape(1, 4)
+    y_pred = model.predict(x)[0][0]
+
+    y_actual = w1 * x[0][0] + w2 * math.pow(x[0][1], 2) + w3 * math.pow(x[0][2], 3) + w4 * math.pow(x[0][3], 4) + b
+
+    print("y actual value = ", y_actual)
+    print("y predicted value = ", y_pred)
+
+
+model = gen_sequential_model()
+X, y = gen_linear_regression_dateset(numofsamples=300000)
+history = model.fit(X, y, epochs=300, verbose=2, validation_split=0.2, batch_size=256)
+plot_loss_curve(history)
+print("train loss=", history.history['loss'][-1])
+print("test loss=", history.history['val_loss'][-1])
+
+predict_new_sample(model, np.array([0.6, 0.3, 0.5, 0.9]))
--- week9/simpleLinearRegressionVisualization.py
+++ week9/simpleLinearRegressionVisualization.py
@@ -1,49 +1,78 @@
+import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import numpy as np
-
-# Data generation
-rng = np.random.RandomState(1)
-X = 10 * rng.rand(200)
-y = 2 * X - 5 + rng.randn(200)
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.set_xlim(X.min() - 1, X.max() + 1)
-ax.set_ylim(y.min() - 1, y.max() + 1)
+import pymysql
 
 
-# cost function using root mean square
-def cost_fun_rmse(X, y, B1, B0):
-    y_pred = X * B1 + B0
-    return np.sum(np.square(y - y_pred)) / X.shape[0]
+def load_dbscore_data():
+    conn = pymysql.connect(host="localhost", user="root", password="6674", db='dataSceince')
+    curs = conn.cursor(pymysql.cursors.DictCursor)
+
+    sql = "select * from score"
+    curs.execute(sql)
+
+    data = curs.fetchall()
+    curs.close()
+    conn.close()
+
+    x = [(t['midterm']) for t in data]
+    x = np.array(x)
+
+    y = [(t['score']) for t in data]
+    y = np.array(y)
+
+    return x, y
 
 
-# Gradient descent
-def gradient_descent(X, y, alpha, B1, B0):
-    y_pred = X * B1 + B0
-    y_diff = (y - y_pred)
-    t_B1 = -2 * (np.sum(y_diff * X) / X.shape[0])
-    t_B0 = -2 * (np.sum(y_diff) / X.shape[0])
-    B1 = B1 - alpha * t_B1
-    B0 = B0 - alpha * t_B0
-    return (B1, B0)
+def main():
+    X, y = load_dbscore_data()
+    model = LinearRegression()
+    model.train(X, y)
+    model.animate(X, y)
 
 
-B1 = 1  # slope
-B0 = 1  # interscept
-alpha = 0.005
-iter_num = 1000  # number of iterations
-x_vals = np.arange(ax.get_xlim()[0], ax.get_xlim()[1], 0.01)
-ax.scatter(X, y, s=2)
-ax.set_title('Linear Regression with Gradient Descent', fontsize=14)
-for i in range(iter_num):
-    ax.legend(['      Slope B1:= ' + str(round(B1, 4)) + '\n' + 'Intercept B0:= ' + str(round(B0, 4))])
-    B1, B0 = gradient_descent(X, y, alpha, B1, B0)
-    y_vals = x_vals * B1 + B0
-    plt_obj = ax.scatter(x_vals, y_vals, marker='x', c='C1', s=1)
-    # time.sleep(0.01)
-    fig.canvas.draw()
-    fig.canvas.flush_events()
-    if i != iter_num - 1:
-        plt_obj.remove()
-plt.close(fig)
+class LinearRegression():
+    def __init__(self, learning_rate=0.001, epochs=100000):
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+        self.min_grad = 0.0001
+        self.c = 0
+        self.m = 0
+        self.w_list = []
+
+    def train(self, X, y):
+        n = len(y)
+        c_grad = 0.0
+        m_grad = 0.0
+
+        for epoch in range(self.epochs):
+            self.w_list.append([self.c, self.m])
+            y_pred = self.m * X + self.c
+            m_grad = (2 * (y_pred - y) * X).sum() / n
+            c_grad = (2 * (y_pred - y)).sum() / n
+
+            self.m = self.m - self.learning_rate * m_grad
+            self.c = self.c - self.learning_rate * c_grad
+            if abs(m_grad) < self.min_grad and abs(c_grad) < self.min_grad:
+                break
+        self.w_list = np.array(self.w_list)
+
+    def animate(self, X, y):
+        fig, ax = plt.subplots()
+        ax.scatter(X, y)
+        plot_range = np.array(range(int(min(X)) - 1, int(max(X)) + 3))
+        a_0, a_1 = self.w_list[0,]
+        y_plot = plot_range * a_1 + a_0
+        ln, = ax.plot(plot_range, y_plot, color="red", label="Best Fit")
+
+        def animator(frame):
+            a_0, a_1 = self.w_list[frame,]
+            y_plot = plot_range * a_1 + a_0
+            ln.set_data(plot_range, y_plot)
+
+        anim = animation.FuncAnimation(fig, func=animator, frames=self.epochs)
+        anim.show()
+
+
+if __name__ == "__main__":
+    main()
